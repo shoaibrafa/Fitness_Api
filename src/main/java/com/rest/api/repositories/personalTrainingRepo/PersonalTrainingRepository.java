6ddf705f1ed9f95ea7a274257c16221c983318e6
ahmadrafa <shoaibrafa@gmail.com>
2022-02-15 19:31:40 +0530

    Modified the PersonalTrainingRepository.java to get the sum of all balances from the database.

--- src/main/java/com/rest/api/repositories/personalTrainingRepo/PersonalTrainingRepository.java
+++ src/main/java/com/rest/api/repositories/personalTrainingRepo/PersonalTrainingRepository.java
@@ -1,11 +1,27 @@
 package com.rest.api.repositories.personalTrainingRepo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.rest.api.model.PersonalTraining;
 
 import java.util.List;
 
 public interface PersonalTrainingRepository extends JpaRepository<PersonalTraining, Integer> {
+	/**
+	 * This method returns all Personal Training a client has taken during his 
+	 * memberships.
+	 * @param id is the client id based on which the method loads all his personal trainings
+	 * @return returns a list of all personal trainings
+	 */
     List<PersonalTraining> findPersonalTrainingByClient_Id(String id);
+    
+    
+    
+    /**
+     * This method calculates all personal training balances from all clients
+     * @return
+     */
+    @Query(value = "SELECT SUM(balance) FROM personal_training", nativeQuery = true)
+    Double sumClientPersonalTrainingBalance();
 }
