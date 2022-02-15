2b883c556d21801fd0b8f442c62f557d849d6aca
ahmadrafa <shoaibrafa@gmail.com>
2022-02-15 19:33:31 +0530

    The DashboardRestController is added return data needed for the dashboard on the client side.

--- src/main/java/com/rest/api/controller/dashboardController/DashboardRestController.java
+++ src/main/java/com/rest/api/controller/dashboardController/DashboardRestController.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.api.repositories.clientMembershipRepo.ClientMembershipRepository;
+import com.rest.api.repositories.personalTrainingRepo.PersonalTrainingRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class DashboardRestController {
 	
 	private ClientMembershipRepository clientMembershipRepository;
+	private PersonalTrainingRepository personalTrainingRepository;
 	
 	@Autowired
-	DashboardRestController(ClientMembershipRepository clientMembershipRepository){
+	DashboardRestController(ClientMembershipRepository clientMembershipRepository,
+							PersonalTrainingRepository personalTrainingRepository){
 		this.clientMembershipRepository = clientMembershipRepository;
+		this.personalTrainingRepository = personalTrainingRepository;
 	}
 	
 	/**
@@ -28,4 +32,16 @@ public class DashboardRestController {
 		Long total = this.clientMembershipRepository.sumClientMembershipBalance();
 		return total;
 	}
+	 
+	/**
+	 * The following method returns the total personal training balance from
+	 * all clients. On the front end the result is displayed on the dash board.
+	 */
+	 @CrossOrigin(origins = "*")
+	 @GetMapping("/dashboard/ptbalance")
+	 public Double totalPersonalTrainingBalance() {
+		 Double total = this.personalTrainingRepository.sumClientPersonalTrainingBalance();
+		 return total;
+	 }
+	 
 }
